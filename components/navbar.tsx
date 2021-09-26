84641cec1cc3bd045ba99d8c830da3af49cfb966
WinteryFox <dragonhel5@gmail.com>
2021-09-26 21:42:31 +0200

    tiny tiny progress

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,17 @@
+import Image from 'next/image'
+
 export default function Navbar() {
     return (
-        <div>
-            Hello this is a header
+        <div className={"sticky top-0 z-40 bg-white flex pl-8 py-5"}>
+            <div className={""}>
+                <a className={"overflow-hidden"} href={"/"}>
+                    <Image className={"w-auto h-10"} src={"/logo_text.svg"} alt={"Logo"} width={200} height={32}/>
+                </a>
+            </div>
+
+            <div>
+                Hello
+            </div>
         </div>
     )
 }
